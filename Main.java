31c254dedcdb11d3192f7b8e39b3875effcde3bb
Skriesten <32945963+Skriesten@users.noreply.github.com>
2025-04-16 21:22:15 -0400

    Create Main.java
    Added a Main java class file

---
+++ Main.java
@@ -0,0 +1,8 @@
+// this is the first class file for the testrepo project repository
+public class Main{
+  public static void main(String args[]){
+  System.out.println("This is the main java file.");
+
+  }
+
+}
